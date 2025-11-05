5e0ff634ad44728fe775911a1bc9a40899817ecd
swiipw <sandra.conde.cuevas@gmail.com>
2025-11-04 20:30:30 -0500

    Create HomeScreen.js

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeScreen = ({ user }) => {
+    return (
+        <div className="p-4">
+            <h2 className="text-2xl font-bold">¡Hola, {user.name}!</h2>
+            <p className="text-gray-600 mt-2">Bienvenido a Workly.</p>
+        </div>
+    );
+};
+
+export default HomeScreen;
