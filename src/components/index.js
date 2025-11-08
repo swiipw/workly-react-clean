23d7ceed01a88887c176c044bdf90b51491e074b
swiipw <sandra.conde.cuevas@gmail.com>
2025-11-08 12:03:02 -0500

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { default as Navbar } from './Navbar';
-export { default as Footer } from './Footer';
-export { default as Features } from './Features';
-export { default as CallToAction } from './CallToAction';
-export { default as Hero } from './Hero';
-export { default as CourseCard } from './CourseCard';
+export * from './Navbar';
+export * from './Footer';
+export * from './Features';
+export * from './CallToAction';
+export * from './Hero';
+export * from './CourseCard';
