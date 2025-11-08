5b6017e3d48db62f2cb9a7092b20e14d4d9be70d
swiipw <sandra.conde.cuevas@gmail.com>
2025-11-08 13:51:05 -0500

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HomeScreen = ({ user }) => {
     return (
-        <div className="p-4">
-            <h2 className="text-2xl font-bold">¡Hola, {user.name}!</h2>
-            <p className="text-gray-600">Bienvenido a Workly.</p>
+        <div className="p-4 animate-fade-in-up">
+            <h2 className="text-3xl font-extrabold text-[#17202A]">¡Hola, {user.name}!</h2>
+            <p className="text-gray-600 mt-2">Tu panel de control.</p>
         </div>
     );
 };
