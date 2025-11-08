fd35e98375efd7f7db43ff0b43e118fbf280097e
swiipw <sandra.conde.cuevas@gmail.com>
2025-11-08 13:42:56 -0500

    Create CoursesScreen.js

---
+++ src/screens/CoursesScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CoursesScreen = () => {
+    return (
+        <div className="p-4">
+            <h2 className="text-2xl font-bold">Cursos</h2>
+            <p className="text-gray-600">Lista de cursos disponibles.</p>
+        </div>
+    );
+};
+
+export default CoursesScreen;
